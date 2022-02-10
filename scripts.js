5f055cae171dc5ca89efeb1e938aa416f78b0e0c
acfernanda <acfernanda98@gmail.com>
2022-02-09 22:35:13 -0300

    mensagens com identificação de cores

--- scripts.js
+++ scripts.js
@@ -1,6 +1,42 @@
 const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-promise.then(processarResposta);
+promise.then(getMessages);
 
-function processarResposta(response) {
-  console.log(response.data);
+function getMessages(response) {
+  const allMessages = response.data;
+
+  for (let i = 0; i < allMessages.length; i++) {
+    const message = allMessages[i];
+    const chatContainer = document.querySelector(".all-messages");
+
+    if (message.type === "message") {
+      chatContainer.innerHTML =
+        chatContainer.innerHTML +
+        `<div class="message-box">
+    <time>(${message.time})</time>
+    <p><span>${message.from}</span> para <span>${message.to}</span>: ${message.text}</p>
+  </div>`;
+    } else if (message.type === "private_message") {
+      chatContainer.innerHTML =
+        chatContainer.innerHTML +
+        `<div class="message-box private">
+    <time>(${message.time})</time>
+    <p><span>${message.from}</span> reservadamente para <span>${message.to}</span>: ${message.text}</p>
+  </div>`;
+    } else if (message.type === "status") {
+      chatContainer.innerHTML =
+        chatContainer.innerHTML +
+        `<div class="message-box login-logout">
+    <time>(${message.time})</time>
+    <p><span>${message.from}</span> ${message.text}</p>
+  </div>`;
+    }
+  }
 }
+
+function scrollToEnd() {
+  const endOfComments = document.querySelector("main");
+  endOfComments.scrollIntoView();
+}
+scrollToEnd();
+
+function sendMessage() {}
