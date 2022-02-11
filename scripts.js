61e35e37fed1fac5d6aec41f16e05982e658d0c0
acfernanda <acfernanda98@gmail.com>
2022-02-10 21:04:21 -0300

    scrolla até a última mensagem

--- scripts.js
+++ scripts.js
@@ -1,42 +1,49 @@
-const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-promise.then(getMessages);
+function getMessages() {
+  const promise = axios.get(
+    "https://mock-api.driven.com.br/api/v4/uol/messages"
+  );
+  promise.then(renderMessages);
+}
 
-function getMessages(response) {
+getMessages();
+
+setInterval(getMessages, 3000);
+
+function renderMessages(response) {
   const allMessages = response.data;
+  let allMessagesInnerHtml = "";
+  const chatContainer = document.querySelector(".all-messages");
 
   for (let i = 0; i < allMessages.length; i++) {
     const message = allMessages[i];
-    const chatContainer = document.querySelector(".all-messages");
 
     if (message.type === "message") {
-      chatContainer.innerHTML =
-        chatContainer.innerHTML +
-        `<div class="message-box">
-    <time>(${message.time})</time>
-    <p><span>${message.from}</span> para <span>${message.to}</span>: ${message.text}</p>
+      allMessagesInnerHtml =
+        allMessagesInnerHtml +
+        `<div data-identifier="message" class="message-box">
+        <p><time>(${message.time})</time>
+    <span>${message.from}</span> para <span>${message.to}</span>: ${message.text}</p>
   </div>`;
     } else if (message.type === "private_message") {
-      chatContainer.innerHTML =
-        chatContainer.innerHTML +
-        `<div class="message-box private">
-    <time>(${message.time})</time>
-    <p><span>${message.from}</span> reservadamente para <span>${message.to}</span>: ${message.text}</p>
+      allMessagesInnerHtml =
+        allMessagesInnerHtml +
+        `<div data-identifier="message" class="message-box private">
+        <p><time>(${message.time})</time>
+    <span>${message.from}</span> reservadamente para <span>${message.to}</span>: ${message.text}</p>
   </div>`;
     } else if (message.type === "status") {
-      chatContainer.innerHTML =
-        chatContainer.innerHTML +
-        `<div class="message-box login-logout">
-    <time>(${message.time})</time>
-    <p><span>${message.from}</span> ${message.text}</p>
+      allMessagesInnerHtml =
+        allMessagesInnerHtml +
+        `<div data-identifier="message" class="message-box login-logout">
+        <p><time>(${message.time})</time>
+    <span>${message.from}</span> ${message.text}</p>
   </div>`;
     }
   }
+  chatContainer.innerHTML = allMessagesInnerHtml;
+  scrollToEnd();
 }
 
 function scrollToEnd() {
-  const endOfComments = document.querySelector("main");
-  endOfComments.scrollIntoView();
+  window.scrollTo(0, document.body.scrollHeight);
 }
-scrollToEnd();
-
-function sendMessage() {}
